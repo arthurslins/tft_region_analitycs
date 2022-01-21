170dea5b06c70e4155ceed72d20250faeb20b4b2
Arthur Lins <arthurlins@id.uff.br>
2022-01-20 22:14:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,22 +15,22 @@ if 'count' not in st.session_state:
 def increment_counter(increment_value=0):
     st.session_state.count += increment_value
 
-st.sidebar.header("Tft Água")
+st.sidebar.header("tft_region_analitycs")
 st.sidebar.image("https://i2.wp.com/gamehall.com.br/wp-content/uploads/2020/03/teamfight-tactics.jpg?fit=1920%2C1080&ssl=1", use_column_width=True)
-pesquisa = st.sidebar.text_input("Coloque um nick para pesquisa")
+pesquisa = st.sidebar.text_input("Inut your nick for search")
 pd.set_option('display.max_colwidth', -1)
 
-st.header ("Team fight tatics Água")
-info= st.checkbox("Informações")
+st.header ("Team fight tatics region analitycs")
+info= st.checkbox("Information")
 
 if info:
-    st.write("Esse aplicativo ainda esta em construção, mas algumas pessoas me pediram e resolvi dar uma adiantada para poder usado.")
-    st.write("Atualmente ele esta contando os jogos e pontuação diária com links do lolchesse mobalytics de todos os players que estiverem no Chall/GM no dia anterior")
-    st.write("Cada dia as 24hrs as tabelas serão resetadas e começara a mesma conta assim por diante.")
-    st.write("Para o futuro eu quero organizar melhor e deixar com um design melhor e adicionar uma parte onde irá ter uma tabela dos snapshots em tempo real")
-    st.write("Eu nunca tinha feito um programa para outras pessoas usarem, então não sei o quanto esse servidor gratuito que hospedei aguenta de acessos simulâneos")
-    st.write("Então, estou disponibilizando como um teste e com o tempo vou vendo o que da para fazer em relação a isso, se houve algum problema")
-    st.write("De resto, espero que isso seja útil de alguma forma, e qualquer feedback só mandar um dm no twitter")
+    st.write("This app is still under construction, but some people asked me and I decided to give it a head start to be able to use it.")
+    st.write("He is currently counting games and daily scores with lolchesse mobalytics links from all players who were in Chall/GM the day before")
+    st.write("Every day at 24hrs the tables will be reset and the same account will start and so on.")
+    st.write("It also has server br snapshot scores updated in real time")
+    st.write("So, I'm making it available as a test and over time I'll see what I can do about it, if there were any problems")
+    st.write("Otherwise, I hope this is useful in some way, and any feedback just send a dm on twitter")
+    st.write(" [Twitter](https://twitter.com/Arthurlinsz)")
     
     
 # def make_clickable(val):
@@ -40,7 +40,7 @@ def main ():
     lista_server=["BR1","EUW1","JP1","KR","NA1"]
 
     server = st.selectbox(
-        'Escolha um server?',
+        'Choose a server?',
         lista_server)
     result=''.join([i for i in server if not i.isdigit()])
 
@@ -63,8 +63,8 @@ def main ():
             games.append(lista_chaa["entries"][i]["wins"]+lista_chaa["entries"][i]["losses"])
 
         df=pd.DataFrame(lp,nick).reset_index().rename(columns={"index":"Nick",0:"League Points"})
-        df["Jogos Diários"]=0
-        df["Jogos"] = games
+        df["Daily Games"]=0
+        df["Games"] = games
         df=df.sort_values("League Points",ascending=False).reset_index(drop=True)  
         nick=[]
 
@@ -80,8 +80,8 @@ def main ():
             games.append(lista_gm["entries"][i]["wins"]+lista_gm["entries"][i]["losses"])
 
         df1=pd.DataFrame(lp,nick).reset_index().rename(columns={"index":"Nick",0:"League Points"})
-        df1["Jogos Diários"]=0
-        df1["Jogos"] = games
+        df1["Daily Games"]=0
+        df1["Games"] = games
         df1=df1.sort_values("League Points",ascending=False).reset_index(drop=True)
         dff=df.append(df1).reset_index(drop=True)
         dff.dropna(inplace=True)
@@ -99,11 +99,11 @@ def main ():
         parcial=pd.DataFrame()
         parcial["Nick"]=df["Nick"]
         parcial["League Points"]=df["League Points_x"]
-        parcial["Jogos Totais"]=df["Jogos_x"]
-        parcial["League Points Diários"]=df["League Points_x"]-df["League Points_y"]
-        parcial["Partidas Diárias"]=df["Jogos_x"]-df["Jogos_y"]
-        parcial=parcial.sort_values("League Points Diários",ascending=False).reset_index(drop=True)
-        parcial.sort_values(['League Points Diários', 'League Points'], ascending=[False, False], inplace=True)
+        parcial["Daily Games"]=df["Games_x"]
+        parcial[" Daily League Points "]=df["League Points_x"]-df["League Points_y"]
+        parcial["Daily Games"]=df["Games_x"]-df["Games_y"]
+        parcial=parcial.sort_values("Daily League Points",ascending=False).reset_index(drop=True)
+        parcial.sort_values(['Daily League Points', 'League Points'], ascending=[False, False], inplace=True)
         # parcial.index += 1
 
         
@@ -117,7 +117,7 @@ def main ():
     parcial,dfo = day(server)
 
 
-    if st.button("Calcular lps diários",on_click=increment_counter,kwargs=dict(increment_value=1)):
+    if st.button("Calculate daily lps",on_click=increment_counter,kwargs=dict(increment_value=1)):
         
 
         parcial["Posição"]=np.arange(parcial.shape[0])
@@ -130,15 +130,15 @@ def main ():
             moba.append(f"https://app.mobalytics.gg/pt_br/tft/profile/{result}/{nick}/overview")
         parcial["lolchess"]=lolchess
         parcial["mobalytics"]=moba
-        cols=["Nick", "League Points Diários", "Partidas Diárias", "League Points", "Jogos Totais","lolchess", "mobalytics"]
+        cols=["Nick", "Daily League Points", "Daily Games", "League Points", "Total Games","lolchess", "mobalytics"]
         parcial = parcial[cols] 
-        parcial.rename(columns={"Jogos Totais": "Partidas Totais"},inplace=True)  
+        parcial.rename(columns={"Total Games": "Total Matches"},inplace=True)  
 #         parcial["League Points Diários"] = parcial["League Points Diários"].astype(int)
         parcial=parcial.dropna()
-        parcial["League Points Diários"]=parcial["League Points Diários"].astype(int)
-        parcial["Partidas Diárias"]=parcial["Partidas Diárias"].astype(int)
+        parcial["Daily League Points"]=parcial["Daily League Points"].astype(int)
+        parcial["Daily Games"]=parcial["Daily Games"].astype(int)
         parcial["League Points"]=parcial["League Points"].astype(int)
-        parcial["Partidas Totais"]=parcial["Partidas Totais"].astype(int)
+        parcial["Total Matches"]=parcial["Total Matches"].astype(int)
         parcial.index+=1
         st.write(parcial[parcial["Nick"]==pesquisa])
         
@@ -171,11 +171,11 @@ def main ():
                     
             snap3=snap3.merge(parcial,how="left",on="Nick")
             # st.write(snap3)
-            snap3["Partidas Totais no snap"]=abs(snap3["Jogos Totais"]-snap3["Partidas Totais"])
+            snap3["Total games in snap"]=abs(snap3["Total Games"]-snap3["Total Matches"])
             snap3.sort_values(by="League Points",ascending=False,inplace=True)
             snap3=snap3.fillna(0)
             # st.write(snap3)
-            column_names=["Nick","Ciclo 1","Ciclo 2","Ciclo 3","Partidas Totais no snap","Soma dos pontos do snap"]
+            column_names=["Nick","Cycle 1","Cycle 2","Cycle 3","Total games in snap","Sum of snapshot points"]
             snap3=snap3[column_names]
             # st.write(snap3)
             snap3=snap3.fillna(0)
@@ -184,23 +184,23 @@ def main ():
             snap3.reset_index(inplace=True,drop=True)
             # st.write(snap3)
             snap3.index=snap3.index+1
-            snap3.loc[1,"Ciclo 4"]=133
-            snap3.loc[2,"Ciclo 4"]=120
-            snap3.loc[3,"Ciclo 4"]=106
-            snap3.loc[4,"Ciclo 4"]=93
-            snap3.loc[5,"Ciclo 4"]=80
-            snap3.loc[6,"Ciclo 4"]=73
-            snap3.loc[7,"Ciclo 4"]=66
-            snap3.loc[8,"Ciclo 4"]=60
-            snap3.loc[9:25,"Ciclo 4"]=47
-            snap3.loc[26:50,"Ciclo 4"]=34
-            snap3.loc[51:100,"Ciclo 4"]=20
-            snap3.loc[101:150,"Ciclo 4"]=8
-            snap3["Soma dos pontos do snap"]= snap3.loc[:,["Ciclo 1","Ciclo 2","Ciclo 3","Ciclo 4"]].sum(axis=1)
-            column_names=["Nick","Ciclo 1","Ciclo 2","Ciclo 3","Ciclo 4","Partidas Totais no snap","Soma dos pontos do snap"]
+            snap3.loc[1,"Cycle 4"]=133
+            snap3.loc[2,"Cycle 4"]=120
+            snap3.loc[3,"Cycle 4"]=106
+            snap3.loc[4,"Cycle 4"]=93
+            snap3.loc[5,"Cycle 4"]=80
+            snap3.loc[6,"Cycle 4"]=73
+            snap3.loc[7,"Cycle 4"]=66
+            snap3.loc[8,"Cycle 4"]=60
+            snap3.loc[9:25,"Cycle 4"]=47
+            snap3.loc[26:50,"Cycle 4"]=34
+            snap3.loc[51:100,"Cycle 4"]=20
+            snap3.loc[101:150,"Cycle 4"]=8
+            snap3["Sum of snapshot points"]= snap3.loc[:,["Cycle 1","Cycle 2","Cycle 3","Cycle 4"]].sum(axis=1)
+            column_names=["Nick","Cycle 1","Cycle 2","Cycle 3","Cycle 4","Total games in snap","Sum of snapshot points"]
             snap3=snap3.reindex(columns=column_names)
             snap3.iloc[:,1:]=snap3.iloc[:,1:].astype(int)
-            snap3=snap3.sort_values(by="Soma dos pontos do snap",ascending=False)
+            snap3=snap3.sort_values(by="Sum of snapshot points",ascending=False)
             snap3.reset_index(drop=True, inplace=True)
             snap3.index=snap3.index+1
 
@@ -215,10 +215,10 @@ def main ():
             # snap.to_csv("snap3.csv")
 
   
-    senha= st.sidebar.text_input("Insira a senha de Admin para atualizar o dia")
+    senha= st.sidebar.text_input("Enter Admin password to update day")
     st.write(senha)   
     if senha == "12345":
-        if st.button("Atualizar o dia"):
+        if st.button("Update the day"):
             troca(dfo) 
             st.session_state.count=0  
             now = datetime.now(tz=tz)
